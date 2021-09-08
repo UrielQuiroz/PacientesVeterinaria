1041011ad3b85261c21edce5b7a706cc597f7e09
Uriel Quiroz <uriel.quiroz187@gmail.com>
2021-09-08 11:10:01 -0500

    Obteniendo datos
    Leyendo los que el usuario escribe en los inputs y pasando los nuevos valores

--- citas/src/components/Formulario.jsx
+++ citas/src/components/Formulario.jsx
@@ -12,9 +12,15 @@ const Formulario = () => {
     })
 
     //Funcion que se ejecuta cada que el usuario escribe en un input
-    const actualizarState = () => {
-        console.log("Kakarotoooooooo..");
+    const actualizarState = e => {
+        actualizarCita({
+            ...cita,
+            [e.target.name]: e.target.value
+        })
     }
+
+    //Extraer los valores 
+    const { mascota, propietario, fecha, hora, sintomas } = cita;
 
     return ( 
         <Fragment>
@@ -23,23 +29,23 @@ const Formulario = () => {
             <form>
                 <label>Nombre mascota</label>
                 <input type="text" name="mascota" className="u-full-width" placeholder="Nombre mascota"
-                        onChange={actualizarState} />
+                        onChange={actualizarState} value={mascota} />
 
                 <label>Nombre del dueño</label>
                 <input type="text" name="propietario" className="u-full-width" placeholder="Nombre del dueño de la mascota"
-                        onChange={actualizarState} />
+                        onChange={actualizarState} value={propietario} />
 
                 <label>Fecha</label>
                 <input type="date" name="fecha" className="u-full-width"
-                        onChange={actualizarState} />
+                        onChange={actualizarState} value={fecha}/>
 
                 <label>Hora</label>
                 <input type="time" name="hora" className="u-full-width"
-                        onChange={actualizarState} />
+                        onChange={actualizarState} value={hora} />
 
                 <label>Sintomas</label>
                 <textarea className="u-full-width" name="sintomas"
-                            onChange={actualizarState}></textarea>
+                            onChange={actualizarState} value={sintomas} ></textarea>
 
                 <button className="u-full-width button-primary">Agregar cita</button>
             </form>
